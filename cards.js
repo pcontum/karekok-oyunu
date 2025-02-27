0510faac2d6c6521d96e585c722e4d00fbb177f6
Ahmet Esat KAYA <147531578+AhmetEsatKAYA@users.noreply.github.com>
2025-02-27 10:54:09 +0300

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,22 +1,22 @@
-var gamename = "Kelime Eşleştirme Oyunu";
+var gamename = "Karekök Oyunu";
 
 var visible_image = true;
 
 var cards = [
-    { word: 'Bilgisayar', meaning: 'Elektronik cihaz, verileri işleyip saklar.', image: ''},
-    { word: 'Telefon', meaning: 'Sesli iletişim aracı.', image: '' },
-    { word: 'Kitap', meaning: 'Yazılı veya basılı sayfalar.', image: '' },
-    { word: 'Ev', meaning: 'İnsanların barındığı yapı.', image: '' },
-    { word: 'Araba', meaning: 'Karayolunda giden motorlu taşıt.', image: '' },
-    { word: 'Bardak', meaning: 'Su içmek için kullanılan nesne.', image: '' },
-    { word: 'Yazı', meaning: 'Harfler ve sembollerle yapılan iletişim şekli.', image: '' },
-    { word: 'Masa', meaning: 'Üzerine eşyaların konulduğu düz yüzey.', image: '' },
-    { word: 'Kapı', meaning: 'Bir yere giriş ve çıkış sağlayan açılabilir nesne.', image: '' },
-    { word: 'Aslan', meaning: 'Bir hayvan, kedigillerden.', image: '' },
-    { word: 'Köpek', meaning: 'Evcil bir hayvan, sadık.', image: '' },
-    { word: 'Saat', meaning: 'Zamanı ölçen araç.', image: '' },
-    { word: 'Pencere', meaning: 'Binalarda ışık almayı sağlayan camlı yapı.', image: '' },
-    { word: 'Çanta', meaning: 'Eşya taşımak için kullanılan kapalı kutu.', image: '' },
-    { word: 'Lamba', meaning: 'Aydınlatma aracı.', image: '' },
-    { word: 'Kamera', meaning: 'Görüntü kaydeden cihaz.', image: '' }
+    { word: '&#8730;20', meaning: '2&#8730;5', image: ''},
+    { word: '&#8730;60', meaning: '2&#8730;15', image: '' },
+    { word: '&#8730;24', meaning: '2&#8730;6', image: '' },
+    { word: '&#8730;180', meaning: '6&#8730;5', image: '' },
+    { word: '&#8730;80', meaning: '4&#8730;5', image: '' },
+    { word: '&#8730;98', meaning: '7&#8730;2', image: '' },
+    { word: '&#8730;125', meaning: '5&#8730;5', image: '' },
+    { word: '&#8730;50', meaning: '5&#8730;2', image: '' },
+    { word: '&#8730;72', meaning: '6&#8730;2', image: '' },
+    { word: '&#8730;8', meaning: '2&#8730;2', image: '' },
+    { word: '&#8730;27', meaning: '3&#8730;3', image: '' },
+    { word: '&#8730;200', meaning: '10&#8730;2', image: '' },
+    { word: '&#8730;48', meaning: '4&#8730;3', image: '' },
+    { word: '&#8730;45', meaning: '3&#8730;5', image: '' },
+    { word: '&#8730;162', meaning: '9&#8730;2', image: '' },
+    { word: '&#8730;243', meaning: '9&#8730;3', image: '' }
 ];
